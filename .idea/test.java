e9080a812fe3d09af4c87ab7112e8e37e7ba34e0
testertest1535 <testertest1535@gmail.com>
2021-09-09 13:17:15 +0300

    test

---
+++ .idea/test.java
@@ -0,0 +1,9 @@
+public class testtttt {
+    public static void main (String [] args){
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
